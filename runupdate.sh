23dcd8b7441a341fbecbe1c9a9e92b29566b6ca3
root <minolink@hotmail.com>
2019-06-17 18:56:22 +0800

    modify directory.

--- runupdate.sh
+++ runupdate.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-d2=$(cat /opt/demoupdate/demoupdate/ver.md)
+d2=$(cat /opt/demoupdate/demoupdate2/ver.md)
 d1=$(cat /home/pi/demoapp/ver.md)
 
 if [ $d2 -gt $d1 ]; then
 	echo "begin update..."
-	unzip -o /opt/demoupdate/demoupdate/ud.zip -d /opt/demoupdate/demoupdate/
+	unzip -o /opt/demoupdate/demoupdate2/ud.zip -d /opt/demoupdate/demoupdate2/
 	mv /home/pi/demoapp/demoapp /home/pi/demoapp/demoapp_bk
-        cp /opt/demoupdate/demoupdate/demoapp /home/pi/demoapp/
+        cp /opt/demoupdate/demoupdate2/demoapp /home/pi/demoapp/
         mv /home/pi/demoapp/ver.md /home/pi/demoapp/ver_bk.md
-        cp /opt/demoupdate/demoupdate/ver.md /home/pi/demoapp	
-	mv /opt/monitor/monitor.py /opt/monitor/monitor_bk.pybk
-	cp /opt/demoupdate/demoupdate/monitor.py /opt/monitor/
+        cp /opt/demoupdate/demoupdate2/ver.md /home/pi/demoapp	
 	echo "update finish"
 	sudo reboot
 fi
